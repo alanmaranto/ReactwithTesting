9b0caadb491dc4e10ea92028a951a99a6da63e3f
alan <alanarostegui@hotmail.com>
2020-07-25 16:00:57 -0500

    events: on click

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Counter from './components/CounterApp';
 
 function App() {
--- src/components/CounterApp.jsx
+++ src/components/CounterApp.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
+  const onAdd = (e) => {
+    console.log(e);
+  };
   return (
     <div>
       <h1>CounterApp</h1>
       <h2>{value}</h2>
+
+      <button onClick={onAdd}>+1</button>
     </div>
   );
 };
