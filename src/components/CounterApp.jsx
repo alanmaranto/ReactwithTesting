61488ffe5d47fd9ba4cfa21309901f00963a8408
alan <alanarostegui@hotmail.com>
2020-07-27 20:09:01 -0500

    use state

--- src/components/CounterApp.jsx
+++ src/components/CounterApp.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(0);
   const onAdd = (e) => {
-    console.log(e);
+    setCounter(counter + 1);
   };
   return (
     <div>
       <h1>CounterApp</h1>
-      <h2>{value}</h2>
+      <h2>{counter}</h2>
 
       <button onClick={onAdd}>+1</button>
     </div>
