cfb411723589de8644e71c98636b9fb722f50238
alan <alanarostegui@hotmail.com>
2021-10-17 18:08:39 -0500

    enzyme configuration from scratch

--- src/test/FirstApp.test.js
+++ src/test/FirstApp.test.js
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 import { render } from "@testing-library/react";
 import FirstApp from "../FirstApp";
 
@@ -8,5 +8,20 @@ describe("Tests in FirstApp Component", () => {
     const { getByText } = render(<FirstApp greeting={greeting} />);
 
     expect(getByText(greeting)).toBeInTheDocument();
+  });
+});
+ */
+
+import React from "react";
+import '@testing-library/jest-dom';
+import { shallow } from "enzyme";
+import FirstApp from "../FirstApp";
+
+describe("Tests in First A", () => {
+  test("should display First App correctly", () => {
+    const greeting = "hello";
+    const wrapper = shallow(<FirstApp greeting={greeting} />);
+
+    expect(wrapper).toMatchSnapshot()
   });
 });
